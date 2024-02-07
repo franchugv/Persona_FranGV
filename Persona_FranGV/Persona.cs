e8d1063f58c8c0365f43a7ca043c1f5c140ae324
DAM 1 <DAM 1@DESKTOP-TKRSI9T>
2024-02-07 10:20:20 +0100

    Agregar archivos de proyecto.

---
+++ Persona_FranGV/Persona.cs
@@ -0,0 +1,41 @@
+﻿namespace Persona_FranGV
+{
+    public enum sexo: byte { H, M };
+
+    public class Persona
+    {
+
+        // MIEMBROS
+
+        private string _nombre;
+
+        private int _edad;
+
+        private string _dni;
+
+        private float _peso;
+
+        private float _altura;
+
+        // CONSTRUCTORES
+
+
+        // PROPIEDADES
+
+        public string Nombre
+        {
+            get
+            {
+                return _nombre;
+            }
+
+            set
+            {
+                _nombre = value;
+            }
+        }
+
+        // MÉTODOS PUBLICOS/PRIVADOS
+
+    }
+}
